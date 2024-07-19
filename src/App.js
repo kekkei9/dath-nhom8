94740ab361771b5bdf7da21cf85482608926a399
kekkei9 <kekkei2002@gmail.com>
2024-07-19 13:22:57 +0700

    update tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Task1 from "./components/Task1/Task1";
-import Task2 from "./components/task9";
+import Task2 from "./components/task2";
 import Task3 from "./components/task9";
 import Task4 from "./components/task9";
 import Task5 from "./components/task9";
@@ -8,7 +8,7 @@ import Task6 from "./components/task9";
 import Task7 from "./components/Task7/Task7";
 import Task8 from "./components/task9";
 import Task9 from "./components/task8";
-import Task10 from "./components/task9";
+import Task10 from "./components/task10";
 
 function App() {
   return (
