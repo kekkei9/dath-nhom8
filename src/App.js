f998d84ade4873fc4a33c3c9677b37583b78c0bd
Lab2k2 <77461736+Lab2k2@users.noreply.github.com>
2024-07-19 22:08:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import Task1 from "./components/Task1/Task1";
 import Task2 from "./components/task2";
-import Task3 from "./components/task9";
+import Task3 from "./components/task3";
 import Task4 from "./components/task4";
-import Task5 from "./components/task9";
+import Task5 from "./components/task5";
 import Task6 from "./components/task6";
 import Task7 from "./components/Task7/Task7";
 import Task8 from "./components/task8";
