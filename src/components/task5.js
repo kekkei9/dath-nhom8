c17ca71fa377c5101fc09563a0ed491824e315be
Lab2k2 <77461736+Lab2k2@users.noreply.github.com>
2024-07-19 22:25:00 +0700

    Update task5.js

--- src/components/task5.js
+++ src/components/task5.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import './SportsBarChart.css';
+import './task3.css';
 
 const data = [
     { category: 'Chơi', listen_music: 0, count: 1.986380882 },
